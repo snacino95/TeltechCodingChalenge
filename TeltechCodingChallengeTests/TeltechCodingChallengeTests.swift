2ad971c06170909556bff9f7624b1c9c5f3bff10
Sergio Nacinovic <snacinovic@dickeys.com>
2022-11-16 17:09:45 +0100

    Adds new testing target

--- TeltechCodingChallengeTests/TeltechCodingChallengeTests.swift
+++ TeltechCodingChallengeTests/TeltechCodingChallengeTests.swift
@@ -2,12 +2,14 @@
 //  TeltechCodingChallengeTests.swift
 //  TeltechCodingChallengeTests
 //
-//  Created by Sergio Načivić on 06.11.2022..
+//  Created by Sergio Načivić on 16.11.2022..
 //
 
 import XCTest
-@testable import TeltechCodingChallenge
+import TeltechCodingChalengeModel
 
 final class TeltechCodingChallengeTests: XCTestCase {
+
     
+
 }
